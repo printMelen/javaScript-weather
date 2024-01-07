4b5d57906dbedd584b407427af9e24a26a13b6a9
printMelen <alvaro.redondofgb@gmail.com>
2024-01-08 00:57:21 +0100

    Funcionando antes de limpiar

--- js/app.js
+++ js/app.js
@@ -16,40 +16,40 @@ let paisStorage = "";
 let ciudadStorage = "";
 let respuesta = "";
 let respuesta3 = "";
-let errorVar = false;
 let http_request = new XMLHttpRequest();
 
-// let paisStorage="ES";
-// let ciudadStorage="Salamanca";
 
 botonEnvio.addEventListener("click", enviarForm);
 
-if (localStorage.getItem("Pais") != null && localStorage.getItem("Ciudad") != null) {
-    request(localStorage.getItem("Ciudad"), localStorage.getItem("Pais"));
-} else {
-    request("Salamanca", "ES");
-}
+//Compruebo que la cookie no es nula
+// if (getCookie("ciudadPais")!= null) {
+    // console.log("BUENAS");
+    enviarForm();
+    // request(localStorage.getItem("Ciudad"), localStorage.getItem("Pais"));
+// } else {
+    //Si la cookie es nula llamo al método enviarForm que se encarga de evaluar la entrada del input y si es necesario crear la cookie por defecto
+    // enviarForm();
+// }
 
 function request(city, country) {
     try {
         http_request.open('GET', "https://api.openweathermap.org/data/2.5/weather?q=" + city + "," + country + "&units=metric&lang=es&appid=" + apiKey, true);
         http_request.onreadystatechange = () => {
             if (http_request.status === 404) {
-                errorVar = true;
                 // Manejar el error 404 específicamente
                 alerta.classList.remove("oculto");
+                //Si la api devuelve error por se una ciudad/pais inexistente elimino la cookie y borro el localStorage
+                deleteCookie("ciudadPais");
                 localStorage.clear();
+                //Se muestra durante 5s que el mensaje de error y luego se oculta
                 setTimeout(function () {
                     alerta.classList.add("oculto");
                 }, 5000);
             } else {
-
                 if (http_request.readyState == 4) {
                     if (http_request.status == 200) {
+                        //Si la respuesta a la petición de la API es correcta parseo la respuesta y la almaceno en una variable
                         respuesta = JSON.parse(http_request.responseText);
-                        // respuesta3 = JSON.stringify(respuesta);
-                        console.log(respuesta);
-                        console.log(respuesta.weather[0].description);
                         dibujarCard();
                     }
                 }
@@ -57,30 +57,79 @@ function request(city, country) {
         };
         http_request.send();
     } catch (error) {
-        console.log("Error: " + error);
+        console.log("Error con la api: " + error);
     }
 }
 
+// Función para obtener el valor de una cookie por su nombre
+function getCookie(cookieName) {
+    // Divide la cadena de cookies en pares clave-valor
+    let cookies = document.cookie.split('; ');
 
+    // Itera a través de los pares y busca el valor de la cookie deseada
+    for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i].split('=');
+        if (cookie[0] === cookieName) {
+            return cookie[1];
+        }
+    }
+
+    // Devuelve null si la cookie no se encuentra
+    return null;
+}
+
+// Función para eliminar una cookie por su nombre
+function deleteCookie(cookieName) {
+    // Configura la fecha de expiración en el pasado
+    let pastDate = new Date(0);
+    
+    // Convierte la fecha de expiración a un formato UTC string
+    let pastDateUTCString = pastDate.toUTCString();
+
+    // Establece la cookie con una fecha de expiración en el pasado, lo que la eliminará
+    document.cookie = cookieName + "=; expires=" + pastDateUTCString + "; path=/";
+}
+
+function crearCookie(city, country){
+    
+    //Creo la cookie con la ciudad y el pais para que dure 1h
+    document.cookie = `ciudadPais=${city},${country}; max-age=7200; path=/`;
+    localStorage.setItem("Ciudad", city);
+    localStorage.setItem("Pais", country);
+}
 function enviarForm() {
+    //Defino el valor de los input
     let pais = document.querySelector("input[name='pais']").value;
     let ciudad = document.querySelector("input[name='ciudad']").value;
     if (ciudad != "" && pais != "") {
+        //Si los inputs no estan vacios limpio el localSotorage y creo la cookie con los valores de los inputs
         localStorage.clear();
-        localStorage.setItem("Ciudad", ciudad);
-        localStorage.setItem("Pais", pais);
+        crearCookie(ciudad,pais);
     } else {
-        localStorage.setItem("Ciudad", "Salamanca");
-        localStorage.setItem("Pais", "ES");
+        //Si estan vacios creo la cookie por defecto.
+        if (getCookie("ciudadPais")== null) {
+            localStorage.clear();
+            crearCookie("Salamanca","ES");
+        }
     }
-    paisStorage = localStorage.getItem("Pais");
-    ciudadStorage = localStorage.getItem("Ciudad");
-    document.cookie = `ciudadPais=${ciudadStorage},${paisStorage}`;
-    request(localStorage.getItem("Ciudad"), localStorage.getItem("Pais"));
+    let cookieValue = getCookie("ciudadPais");
 
+    if (cookieValue !== null) {
+        //Divido la ciudad del pais
+        let valorCookie=cookieValue.split(',');
+        //Los guardo en el localStorage
+        localStorage.setItem("Ciudad", valorCookie[0]);
+        localStorage.setItem("Pais", valorCookie[1]);
+        //Y llamo a request con los valores de la cookie
+        request(valorCookie[0], valorCookie[1]);
+    } else {
+        console.log("La cookie no se encontró o ha expirado.");
+    }
 }
 function dibujarCard() {
+    //Muestro la imagen de la api
     img.setAttribute("src", "http://openweathermap.org/img/w/" + respuesta.weather[0].icon + ".png");
+    //Relleno la información de la tarjeta según los datos de la api.
     ciudadyPais.textContent = respuesta.name + "," + respuesta.sys.country;
     let fechaSinForm = moment.unix(respuesta.dt).format('LLLL');
     let salida1 = moment.unix(respuesta.sys.sunrise).format('LTS');
@@ -93,8 +142,4 @@ function dibujarCard() {
     humedad.textContent = "Humedad: " + respuesta.main.humidity + "%";
     salida.textContent = "Salida: " + salida1;
     puesta.textContent = "Puesta: " + puesta1;
-}
-function error() {
-    alerta.classList.remove("oculto");
-    alerta.classList.add("oculto");
 }
